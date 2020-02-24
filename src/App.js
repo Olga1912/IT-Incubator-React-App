fdc70115212bf3bc32a2f9b7791d0ea7b374fe87
Olga Volosevich <olga.lucky.v@gmail.com>
2020-02-24 22:57:40 +0300

    feat: add basic layout

--- src/App.js
+++ src/App.js
@@ -1,26 +1,47 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="app-wrapper">
+      <header className="header">
+        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2f/Logo_TV_2015.svg/1200px-Logo_TV_2015.svg.png" alt="logo image"/>
       </header>
+      <nav className="nav">
+        <ul>
+          <li><a href="#">Profile</a></li>
+          <li><a href="#">Messages</a></li>
+          <li><a href="#">News</a></li>
+          <li><a href="#">Music</a></li>
+          <li><a href="#">Settings</a></li>
+        </ul>
+      </nav>
+      <div className="content">
+        <div>
+          <img src="https://sky-wall.ru/uploads/source/2014/12/preview/a867fe1ce6706a9fbfd45bc4267e9351.jpg" alt="default content image"/>
+        </div>
+        <div>
+          ava + description
+        </div>
+        <div>
+          my posts
+          <div>
+            new post
+          </div>
+          <div>
+            <div>
+              post 1
+            </div>
+            <div>
+              post 2
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
+
 
 export default App;
